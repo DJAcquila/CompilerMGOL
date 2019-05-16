f8c373fe714343c32e586ce9e6ba72f4a80985f9
DJAcquila <acquila.santos@gmail.com>
2019-05-16 11:40:37 -0300

    FODASSE

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-
+import pandas as pd
 from lexico.analisadorlexico import parse
+from sintatico.syn import Shift_Reduce
 from common.utility.util import bcolors
 from common.file.fileHandler import FileHandler
 import argparse
@@ -12,6 +13,7 @@ if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description="Compilador da linguagem MGOL - Por enquanto, apenas o analisador léxico")
 	parser.add_argument('-l', '--lexico', help='Realiza somente a analise léxica', action="store_true")
 	parser.add_argument('-v', '--verbose', help='Ativa o modo verboso do compilador', action="store_true")
+	parser.add_argument('-s', '--sintatico', help='Realiza somente a analise lexica e sintatica', action="store_true")
 	parser.add_argument('filename')
 	args = parser.parse_args()
 	if args.filename.find('.alg') < 0:
@@ -20,6 +22,14 @@ if __name__ == "__main__":
 		exit(0)
 
 	file = FileHandler(args.filename)
+	tabela_acoes = pd.read_csv("sintatico/tabela-sintatica-acoes.csv")
+	#print(tabela_acoes.loc[3]['id'])
+
+	tabela_desvios = pd.read_csv("sintatico/tabela-sintatica-desvios.csv")
+	#print (tabela_desvios.loc[0]['P'])
+
+	#gramatica, usadas no algoritmo para o reduce
+	regras = pd.read_csv("sintatico/gramatica.csv")
 
 	'''_arquivo = open(args.filename, 'r')
 		
@@ -30,6 +40,9 @@ if __name__ == "__main__":
 		if args.verbose:
 			print ("Modo verboso ativado")
 		parse(file, args.verbose)
+
+	if args.sintatico:
+		Shift_Reduce(file, tabela_acoes, tabela_desvios, regras)
 	else:
 		print("Ainda não foram desenvolvidas todas as etapas de compilação\nInsira a diretiva '-l' para execução do analisador léxico")
 	
--- sintatico/syn.py
+++ sintatico/syn.py
@@ -3,15 +3,11 @@
 
 import pandas as pd
 import string
-
-tabela_acoes = pd.read_csv("tabela-sintatica-acoes.csv")
-#print(tabela_acoes.loc[3]['id'])
-
-tabela_desvios = pd.read_csv("tabela-sintatica-desvios.csv")
-#print (tabela_desvios.loc[0]['P'])
-
-#gramatica, usadas no algoritmo para o reduce
-regras = pd.read_csv("gramatica.csv")
+from common.utility.util import *
+from common.erro.errno import Error
+from common.symbtable.table import SymbTable
+from common.file.fileHandler import FileHandler
+from lexico.analisadorlexico import *
 
 class Pilha(object):
 	def __init__(self):
@@ -27,32 +23,111 @@ class Pilha(object):
 	def vazia(self):
 		return len(self.dados) == 0
 
-def Shift_Reduce():
+	def topo(self):
+		indice = len(self.dados) - 1
+		return self.dados[indice]
+
+# def Shift_Reduce():
+# 	pilha = Pilha()
+# 	pilha.empilha(18)
+# 	a = 'id' #simbolo do lexico
+# 	while True:
+# 		s = int(pilha.desempilha())
+# 		if 's' in tabela_acoes.loc[s][a]:
+# 			print('acao shift')
+# 			t = tabela_acoes.loc[s][a]
+# 			t = t.split('s')
+# 			t = int(t[1])
+# 			pilha.empilha(int(t))
+# 			#a = #proximo simbolo de entrada
+# 		elif 'r' in tabela_acoes.loc[s][a]:
+# 			print('acao reduce')
+# 			red = tabela_acoes.loc[s][a]
+# 			red = red.split('r')
+# 			red = int(red[1])
+# 			B_simbols = int(regras.loc[red]['B_number'])
+# 			while (B_simbols > 0):
+# 				print(pilha.desempilha())
+# 				B_simbols-= B_simbols
+# 			t = pilha.desempilha()
+# 			A = regras.loc[red]['Antecedente']
+# 			print(A)
+# 			pilha.empilha(tabela_desvios.loc[t][A])
+# 			print(regras.loc[red]['Antecedente']+'->'+regras.loc[red]['Consequente'])
+
+# 		elif 'acc' in tabela_acoes.loc[s][a]:
+# 			print('aceita')
+# 			break
+# 		else:
+# 			print('erro')
+
+def Shift_Reduce(file, tabela_acoes, tabela_desvios, regras):
+
+	lex = LEX_DFA(file)
+	tabela_simbolos = SymbTable()
+
 	pilha = Pilha()
 	pilha.empilha(0)
-	a = 'id' #simbolo do lexico
+	#a = 'id' #simbolo do lexico
+	try:
+		accept = False, None, None, None
+		if (file.ponteiro < file.eof):
+			#p = int(tok)
+			accept = lex.dfa.lexico(tabela_simbolos)
+			if accept[0] != 'erro':
+				#print('\nLexema: {}\nToken: {}\nTipo: {}\n'.format(accept[1],accept[2],accept[3]))
+				a = accept[2]
+				print(a)
+
+		else:
+			a='$'
+	except TypeError:
+		pass
+
 	while True:
-		s = int(pilha.desempilha())
+		print("Inicio: {}".format(pilha.dados))
+		s = int(pilha.topo())
+		print('topo: {}'.format(s))
 		if 's' in tabela_acoes.loc[s][a]:
 			print('acao shift')
 			t = tabela_acoes.loc[s][a]
 			t = t.split('s')
 			t = int(t[1])
+			print(t)
 			pilha.empilha(int(t))
 			#a = #proximo simbolo de entrada
+			try:
+				accept = False, None, None, None
+				if (file.ponteiro < file.eof):
+					#p = int(tok)
+					accept = lex.dfa.lexico(tabela_simbolos)
+					if accept[0] != 'erro':
+						#print('\nLexema: {}\nToken: {}\nTipo: {}\n'.format(accept[1],accept[2],accept[3]))
+						a = accept[2]
+						#print(a)
+
+				else:
+					a='$'
+			except TypeError:
+				pass
+			print(a)
 		elif 'r' in tabela_acoes.loc[s][a]:
 			print('acao reduce')
 			red = tabela_acoes.loc[s][a]
 			red = red.split('r')
 			red = int(red[1])
 			B_simbols = int(regras.loc[red]['B_number'])
+			print(B_simbols)
 			while (B_simbols > 0):
-				print(pilha.desempilha())
-				B_simbols-= B_simbols
-			t = pilha.desempilha()
-			A = regras.loc[red]['Antecedente']
-			print(A)
-			pilha.empilha(tabela_desvios.loc[t][A])
+				pilha.desempilha()
+				print("Depois do desempilha: {}".format(pilha.dados))
+				B_simbols-= 1
+			print("Depois do while: {}".format(pilha.dados))
+			t = pilha.topo()
+			print('topo: {}'.format(t))
+			Ant = regras.loc[red]['Antecedente']
+			print(Ant)
+			pilha.empilha(tabela_desvios.loc[t][Ant])
 			print(regras.loc[red]['Antecedente']+'->'+regras.loc[red]['Consequente'])
 
 		elif 'acc' in tabela_acoes.loc[s][a]:
@@ -60,12 +135,13 @@ def Shift_Reduce():
 			break
 		else:
 			print('erro')
+			break
 
 #while True:
 #	topo_pilha_s = 
 #	if ACTION(topo_pilha_s,token_a) == st:
 #		empilha(t)
 		
-if __name__ == "__main__":
+#if __name__ == "__main__":
 
-	Shift_Reduce()
+#	Shift_Reduce()
