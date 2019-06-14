2425afd50b4ff5503d008304b6cb735bf36369cf
Julianameloo <msjulianamelo@gmail.com>
2019-06-13 22:34:14 -0300

    b

--- programa.c
+++ programa.c
@@ -1,21 +1,24 @@
+lit A
 inteiro B
 inteiro c
 
 
 
-printf(""Digite um nome:"");
-printf(""Digite um numero:"");
-scanf("%d",B)B=2
-B=3
-T0B + 1
-c=T0
-T1c + 1
-c=T1
-T2B < 1
+printf("Digite um nome:");
+scanf("%s",A)
+printf("Digite um numero:");
+scanf("%d",B)
+B = 2
+B = 3
+T0 = B + 1
+c = T0
+T1 = c + 1
+c = T1
+T2 = B < 1
 if(T2){
-T3c < 2
+T3 = c < 2
 if(T3){
-printf(""texto"");
+printf("texto");
 }
 }
-printf(""programa finalizado"");
+printf("programa finalizado");
